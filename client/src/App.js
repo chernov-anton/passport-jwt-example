7a3d2cded39aab5bbd5dd326f827f49435b851a2
Anton Chernov <chernov.anton.dev@gmail.com>
2019-08-02 06:36:37 +0300

    feat: add sticky footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import Routes from './routes';
 import Header from './components/header';
 import Footer from './components/footer';
 
+import 'app.css';
+
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes/>
-      <Footer/>
+      <div className="app-container">
+        <div>
+          <Header/>
+          <Routes/>
+        </div>
+        <Footer/>
+      </div>
     </BrowserRouter>
   );
 }
